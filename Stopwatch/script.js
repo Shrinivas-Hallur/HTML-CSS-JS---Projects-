77e0bdf1afe40ad6524b7d625cd37cdaa09275ee
Shrinivas-Hallur <shrinivasphallur@gmail.com>
2026-01-24 21:44:04 +0530

    commiting html and css of stopwatch project

---
+++ Stopwatch/script.js
@@ -0,0 +1,9 @@
+const display=document.querySelector('#result');
+const startbtn=document.querySelector('#start-btn');
+const stopbtn=document.querySelector('#stop-btn');
+const resetbtn=document.querySelector('#reset-btn');
+const flagbtn=document.querySelector('#flag-btn');
+
+startbtn.addEventListener('click',()=>{
+    
+});
