d2a26a4304b0a73c72446f71d83370ce821d72c2
Shrinivas-Hallur <shrinivasphallur@gmail.com>
2026-01-24 22:17:31 +0530

    added start stop reset fucntionalities to stopwatch

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -4,6 +4,40 @@ const stopbtn=document.querySelector('#stop-btn');
 const resetbtn=document.querySelector('#reset-btn');
 const flagbtn=document.querySelector('#flag-btn');
 
+let interval;
+let running=false;
+let startTime=0;
+
+const padding=(num)=>num.toString().padStart(2,'0');
+const milliPadding=(num)=>num.toString().padStart(3,'0');
+
 startbtn.addEventListener('click',()=>{
-    
+    if(startTime==0){
+        startTime=Date.now();
+    }
+
+    if(!running){
+        running=true;
+        interval=setInterval(()=>{
+            let timeGap=Date.now()-startTime
+            let inMilliSeconds=timeGap%1000;
+            let inSeconds=Math.floor(timeGap/1000)
+            let inMinute=Math.floor(timeGap/1000/60)
+            if(inSeconds>=60)inSeconds%=60;
+
+            display.textContent=`${padding(inMinute)}:${padding(inSeconds)}:${padding(inMilliSeconds)}`
+         },15);
+    }
+});
+
+stopbtn.addEventListener('click',()=>{
+    running=false;
+    clearInterval(interval)
+});
+
+resetbtn.addEventListener('click',()=>{
+     running=false;
+     clearInterval(interval);
+     startTime=0;
+     display.textContent="00:00:000";
 });
