8b9e4aee2b564ef7c6ee74a781b8b606284d9d3b
Shrinivas-Hallur <shrinivasphallur@gmail.com>
2026-01-25 18:32:39 +0530

    implemented lap function

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -1,4 +1,5 @@
 const display=document.querySelector('#result');
+const lapsContainer=document.querySelector('#laps');
 const startbtn=document.querySelector('#start-btn');
 const stopbtn=document.querySelector('#stop-btn');
 const resetbtn=document.querySelector('#reset-btn');
@@ -7,14 +8,16 @@ const flagbtn=document.querySelector('#flag-btn');
 let interval;
 let running=false;
 let startTime=0;
+let lapCount=1;
+let elapsedTime=0;
 
 const padding=(num)=>num.toString().padStart(2,'0');
 const milliPadding=(num)=>num.toString().padStart(3,'0');
 
 startbtn.addEventListener('click',()=>{
-    if(startTime==0){
-        startTime=Date.now();
-    }
+    // if(startTime==0){
+        startTime=Date.now()-elapsedTime;
+    // }
 
     if(!running){
         running=true;
@@ -25,13 +28,15 @@ startbtn.addEventListener('click',()=>{
             let inMinute=Math.floor(timeGap/1000/60)
             if(inSeconds>=60)inSeconds%=60;
 
-            display.textContent=`${padding(inMinute)}:${padding(inSeconds)}:${padding(inMilliSeconds)}`
+            display.textContent=`${padding(inMinute)}:${padding(inSeconds)}:${milliPadding(inMilliSeconds)}`
          },15);
     }
 });
 
 stopbtn.addEventListener('click',()=>{
     running=false;
+
+    elapsedTime=Date.now()-startTime;
     clearInterval(interval)
 });
 
@@ -39,5 +44,17 @@ resetbtn.addEventListener('click',()=>{
      running=false;
      clearInterval(interval);
      startTime=0;
+     elapsedTime=0;
+     lapCount=1;
      display.textContent="00:00:000";
+     lapsContainer.innerHTML="";
+});
+
+flagbtn.addEventListener('click',()=>{
+    if(!running) return;
+
+    const lap=document.createElement('div');
+    lap.textContent=`Lap ${lapCount}: ${display.textContent}`;
+    lapsContainer.appendChild(lap);
+    lapCount++;
 });
